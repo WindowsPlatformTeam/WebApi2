10bb1a58153e7370fdd60fcc0c729e1e1dd02471
acnagrellos <acnagrellos@gmail.com>
2017-01-10 16:07:02 +0100

    Starter Project (#3)
    * Boiler Plate
    
    Update image webapi
    
    * Add references and conclusions
    
    Add references and conclusions
    
    Remove reference github code
    
    Fix <> characters in Readme

---
+++ 00 BoilerPlate/src/Api.Core/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Core
+{
+    public static class Constants
+    {
+        public const string ApiName = "API";
+
+        public const string BaseRoute = "api";
+
+        public const string Test = "test";
+        public const string GetTestBoolean = "get-test-boolean";
+        public const string GetTestBooleanWithParam = "get-test-boolean-with-param/{id}";
+        public const string PostTestBoolean = "post-test-boolean";
+    }
+}
---
+++ 00 BoilerPlate/src/Api.Core/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Api.Core.Controllers.Base;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Api.Core.Controllers
+{
+    [RoutePrefix(Constants.BaseRoute + "/" + Constants.Test)]
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        [ResponseType(typeof(bool))]
+        [Route(Constants.GetTestBoolean)]
+        public async Task<IHttpActionResult> GetTestBoolean()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(bool))]
+        [Route(Constants.GetTestBooleanWithParam)]
+        public async Task<IHttpActionResult> GetTestBooleanWithParam(int id)
+        {
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(bool))]
+        [Route(Constants.PostTestBoolean)]
+        public async Task<IHttpActionResult> PostTestBoolean()
+        {
+            return Ok(true);
+        }
+    }
+}
+
